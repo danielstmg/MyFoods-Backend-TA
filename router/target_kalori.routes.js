2182ee901ac555d12cd8f9dd9416de0e3f423d39
Daniel <sdanielsahat1632@gmail.com>
2024-08-05 23:09:49 +0700

    add routes target_kalori

---
+++ router/target_kalori.routes.js
@@ -0,0 +1,16 @@
+var router = require('express').Router()
+var TargetKalori = require('../controller/target_kalori.controller')
+
+// get all target kalori
+router.get('/all_target_kalori', TargetKalori.getAllTargetKalori)
+
+// get target kalori user
+router.get('/target_kalori', TargetKalori.getTargetKalori)
+
+// get target kalori user by hari
+router.get('/target_kalori/:hari', TargetKalori.getTargetKaloriByHari)
+
+// set target kalori user
+router.post('/target_kalori', TargetKalori.setTargetKalori)
+
+module.exports = router
